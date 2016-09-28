0298a26aa46f2ad7ea46207c6d621e428d1b4082
Tim Meusel <tim@bastelfreak.de>
2016-09-29 00:13:25 +0200

    implement new modular structure

--- weekly_reports/magicshellscript.sh
+++ weekly_reports/magicshellscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-AUTHORS=(bastelfreak hrnz somedude)
+AUTHORS=(bastelfreak skarf marcellii)
 WEEKS=(2016_37)
 
 # iterate at all weeky directories
@@ -8,11 +8,13 @@ for directory in ${WEEKS[*]}; do
   # iterate at each author
   for author in ${AUTHORS[*]}; do
     if [ -f "${author}.tex" ]; then
-      sed --in-place "s/###author###/$author/" content.tex main.tex
+      sed --in-place "s|###author###|$author|" main.tex
+      sed --in-place "s|###author###|$directory/$author|" ../content.tex
       latexmk -C
       rm "${directory}_${author}.pdf"
       latexmk -jobname="${directory}_${author}" -r ../latexmkrc
-      sed --in-place "s/$author/###author###/" content.tex main.tex
+      sed --in-place "s|$author|###author###|" main.tex
+      sed --in-place "s|$directory/$author|###author###|" ../content.tex
     fi
   done
 done
