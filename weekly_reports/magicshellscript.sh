33897887ab9c0642682ef93ef914a9f4355d9ee5
Tim Meusel <tim@bastelfreak.de>
2016-11-21 18:10:03 +0100

    update shellscript for monthly reports

--- weekly_reports/magicshellscript.sh
+++ weekly_reports/magicshellscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-AUTHORS=(bastelfreak skarf marcellii)
+AUTHORS=(bastelfreak skarf marcellii monatsbericht)
 WEEKS=(2016_37 2016_38 2016_39 2016_40 2016_41 2016_42 2016_43 2016_44 2016_45)
 
 # iterate at all weeky directories
