7da41ebad98d36ec5489b3aeba4453d83afa547e
Nikolai Desktop <StoneCat@luis.is>
2017-02-19 14:39:44 +0100

    Add Test Procedure

--- sql_sample/003_Partition_Model.sql
+++ sql_sample/003_Partition_Model.sql
@@ -61,9 +61,51 @@ CREATE TRIGGER insert_measurement_trigger
     BEFORE INSERT ON measurement_master
     FOR EACH ROW EXECUTE PROCEDURE measurement_insert_trigger();
 
+-- TEST
+DROP function if exists convert_input_data_to_json_measurement_table(integer);
+
+CREATE OR REPLACE FUNCTION public.convert_input_data_to_json_measurement_table(integer)
+  RETURNS character AS $BODY$
+DECLARE
+	r log%ROWTYPE;
+
+	id_count integer := 0;
+ BEGIN
+	FOR r IN
+		SELECT *
+		FROM log l
+		ORDER BY l."timestamp"
+		LIMIT $1
+	LOOP
+		INSERT INTO measurement_master VALUES (id_count,
+				json_build_object(
+					'insert_at', current_timestamp::timestamp,
+					'aggregation_type', 'seconds'),
+				json_build_object(
+					'host',r.host,
+					'timestamp',r."timestamp",
+					'plugin',r.plugin,
+					'type_instance',r.type_instance,
+					'collectd_type',r.collectd_type,
+					'plugin_instance',r.plugin_instance,
+					'type',r.type,
+					'value',r.value,
+					'version',r.version)
+				);
+		id_count := id_count + 1;
+	END LOOP;
+	
+	return 'Running ' || id_count || ' Times!';
+	--- Do not user '. Times!' -> the point tells postgresSQL, that the string is a table-column
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION public.convert_input_data_to_json_measurement_table(integer)
+  OWNER TO metrics;
+
+
 -- NOTES
-
-
 ---- Building CHECK;
 -- CHECK ( logdate >= DATE '2006-02-01' AND logdate < DATE '2006-03-01' )
 -- add: measurement_master
