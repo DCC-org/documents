47a3a92ebf4e79675330ab325ab44e145e9f271f
Nikolai Desktop <StoneCat@luis.is>
2017-04-29 23:45:36 +0200

    Add procedure to check if last value same as new

--- sql/004_data_aggregation_at_etll.sql
+++ sql/004_data_aggregation_at_etll.sql
@@ -1,10 +1,7 @@
--- 2017-04-29 13:25:58.159398
-select *
-from test_me
-where zeit <= to_timestamp('2017-04-29 13:25:58', 'YYYY-MM-DD HH24:MI:SS')::timestamp
-order by zeit DESC
-LIMIT 1;
+current_setting(from_value)
+CREATE TYPE return_type AS (is_same boolean, foundtimestamp timestamp, foundvalue text, foundrowid bigint );
 
+DROP FUNCTION public.check_if_last_value_is_same(timestamp,TEXT,TEXT);
 
 CREATE OR REPLACE FUNCTION public.check_if_last_value_is_same
 	(
@@ -12,7 +9,7 @@ CREATE OR REPLACE FUNCTION public.check_if_last_value_is_same
 	etlmetaid TEXT,
 	value_new TEXT
 	)
-RETURNS BOOLEAN AS $BODY$
+RETURNS return_type AS $BODY$
 DECLARE
 	prefix text := 'partitions';
 	partition text;
@@ -20,20 +17,24 @@ DECLARE
 	foundtimestamp timestamp;
 	foundrowid bigint;
 	foundvalue TEXT;
+	
+	result_record return_type;
 BEGIN
-	partition := substring(zeit_new from 1 for 13);
+	partition := substring(zeit_new::text from 1 for 13);
 	partition := replace(partition,'-', '_');
 	partition := replace(partition,' ', 't');
 	
-	from_value := prefix || '.' || partition;
+	from_value := prefix || '.measurement_' || partition;
+	
+	RAISE NOTICE '%', from_value;
 
 	SELECT to_timestamp(data->>'timestamp'::text, 'YYYY-MM-DD HH24:MI:SS')::timestamp,
 			id,
 			data->>'value'::text
 	INTO
-			foundtimestamp, foundrowid, foundvalue
+			result_record.foundtimestamp, result_record.foundrowid, result_record.foundvalue
 	FROM
-		from_value
+		:from_value
 	WHERE
 		metadata->>'etlid'::text = "etlmetaid"
 	AND
@@ -44,14 +45,16 @@ BEGIN
 	LIMIT 1;
 	
 	IF value_new = foundvalue THEN
-		return true;
+		result_record.is_same := true;
 	ELSE
-		return false;
+		result_record.is_same := false;
 	END IF;
+	
+	return result_record;
 
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
   
-public.check_if_last_value_is_same('2017-02-15 20:52:43'::timestamp, '13'::text, '')
+select public.check_if_last_value_is_same('2017-02-15 20:52:43'::timestamp, '13'::text, '0');
