0046577f3622fd6fd08523760beba8b03887426e
Nikolai Desktop <StoneCat@luis.is>
2017-05-01 17:20:42 +0200

    api select with max 2 hours

--- sql/005_api_select_functions.sql
+++ sql/005_api_select_functions.sql
@@ -13,8 +13,8 @@ DECLARE
   prefix TEXT := 'partitions';
   temp_table TEXT := 'tmp_' || md5(''||now()::text||random()::text)::text;
   
-  anz_data int;
   etlid TEXT;
+  this_record RECORD;
 BEGIN
   EXECUTE 'SELECT string_agg(etldata->>''etlid'', ''", "'') FROM public.etl_master WHERE datacontent->>''host'' = ''' || in_hostname || ''' AND datacontent->>''plugin'' = ''' || in_plugin || ''' AND datacontent->>''type_instance'' = ''' || in_type_instance || ''' ;' into etlid;
   etlid := '"' || etlid || '"';
@@ -34,27 +34,29 @@ BEGIN
   partition_end := replace(partition_end,' ', 't');
   partition_end := 'measurement_' || partition_end;
   
-  IF partition_start = partition_end THEN
-    RAISE NOTICE '% % % % % %', in_hostname, in_plugin, in_start_time, in_end_time, partition_start, partition_end;
-    
-	EXECUTE 'create temporary table ' || temp_table || ' (etlid TEXT, out_timestamp int, out_value float, out_plugin_instance TEXT, out_collectd_type TEXT) on commit drop;';
-	
-	EXECUTE 'INSERT INTO ' || temp_table || ' SELECT metadata->>''etlid'', extract(epoch from to_timestamp(data->>''timestamp''::text, ''YYYY-MM-DD HH24:MI:SS'')::timestamp)::int, CAST(data->>''value'' AS FLOAT)::float, data->>''plugin_instance''::text, data->>''collectd_type''::text FROM ' || prefix || '.' || partition_start || ' WHERE metadata->>''etlid'' in (' || etlid || ') AND to_timestamp(data->>''timestamp''::text, ''YYYY-MM-DD HH24:MI:SS'')::timestamp <= ''' || format_end_time || '''::timestamp AND to_timestamp(data->>''timestamp''::text, ''YYYY-MM-DD HH24:MI:SS'')::timestamp >= ''' || format_start_time || '''::timestamp ORDER BY data->>''timestamp'';'; 
-	
+   
+  EXECUTE 'create temporary table ' || temp_table || ' (etlid TEXT, out_timestamp int, out_value float, out_plugin_instance TEXT, out_collectd_type TEXT) on commit drop;';
+
+  EXECUTE 'INSERT INTO ' || temp_table || ' SELECT metadata->>''etlid'', extract(epoch from to_timestamp(data->>''timestamp''::text, ''YYYY-MM-DD HH24:MI:SS'')::timestamp)::int, CAST(data->>''value'' AS FLOAT)::float, data->>''plugin_instance''::text, data->>''collectd_type''::text FROM ' || prefix || '.' || partition_start || ' WHERE metadata->>''etlid'' in (' || etlid || ') AND to_timestamp(data->>''timestamp''::text, ''YYYY-MM-DD HH24:MI:SS'')::timestamp <= ''' || format_end_time || '''::timestamp AND to_timestamp(data->>''timestamp''::text, ''YYYY-MM-DD HH24:MI:SS'')::timestamp >= ''' || format_start_time || '''::timestamp ORDER BY data->>''timestamp'';'; 
+  
+  IF partition_start != partition_end THEN
+    EXECUTE 'INSERT INTO ' || temp_table || ' SELECT metadata->>''etlid'', extract(epoch from to_timestamp(data->>''timestamp''::text, ''YYYY-MM-DD HH24:MI:SS'')::timestamp)::int, CAST(data->>''value'' AS FLOAT)::float, data->>''plugin_instance''::text, data->>''collectd_type''::text FROM ' || prefix || '.' || partition_end || ' WHERE metadata->>''etlid'' in (' || etlid || ') AND to_timestamp(data->>''timestamp''::text, ''YYYY-MM-DD HH24:MI:SS'')::timestamp <= ''' || format_end_time || '''::timestamp AND to_timestamp(data->>''timestamp''::text, ''YYYY-MM-DD HH24:MI:SS'')::timestamp >= ''' || format_start_time || '''::timestamp ORDER BY data->>''timestamp'';'; 
   END IF;
   
-  EXECUTE 'SELECT COUNT(*)::int as anz from ' || temp_table || ';' into anz_data;
-  RAISE NOTICE '%', anz_data;
-  
-  IF anz_data = 0 THEN
-	EXECUTE 'INSERT INTO ' || temp_table || ' SELECT metadata->>''etlid'', extract(epoch from to_timestamp(data->>''timestamp''::text, ''YYYY-MM-DD HH24:MI:SS'')::timestamp)::int, CAST(data->>''value'' AS FLOAT)::float, data->>''plugin_instance''::text, data->>''collectd_type''::text FROM ' || prefix || '.' || partition_start || ' WHERE metadata->>''etlid'' in (' || etlid || ') AND to_timestamp(data->>''timestamp''::text, ''YYYY-MM-DD HH24:MI:SS'')::timestamp <= ''' || format_start_time || '''::timestamp ORDER BY data->>''timestamp'' LIMIT 1;';
+  FOR this_record IN
+  EXECUTE 'SELECT COUNT(*)::int as anz_data, etlid as etl_id from ' || temp_table || ' group by etlid;' LOOP
+	RAISE NOTICE '% FOR %', this_record.anz_data, this_record.etl_id;
 	
-	RETURN QUERY
-	EXECUTE 'SELECT ''' || in_start_time || '''::int, out_value, out_plugin_instance, out_collectd_type FROM ' || temp_table || ';';
-  ELSE
-    RETURN QUERY
-    EXECUTE 'SELECT out_timestamp, out_value, out_plugin_instance, out_collectd_type FROM ' || temp_table || ';';
-  END IF;
+    IF this_record.anz_data = 0 THEN
+	  EXECUTE 'INSERT INTO ' || temp_table || ' SELECT metadata->>''etlid'', extract(epoch from to_timestamp(data->>''timestamp''::text, ''YYYY-MM-DD HH24:MI:SS'')::timestamp)::int, CAST(data->>''value'' AS FLOAT)::float, data->>''plugin_instance''::text, data->>''collectd_type''::text FROM ' || prefix || '.' || partition_start || ' WHERE metadata->>''etlid'' in (' || etlid || ') AND to_timestamp(data->>''timestamp''::text, ''YYYY-MM-DD HH24:MI:SS'')::timestamp <= ''' || format_start_time || '''::timestamp AND etlid = ''' || this_record.etl_id || ''' ORDER BY data->>''timestamp'' LIMIT 1;';
+	  
+	  RETURN QUERY
+	  EXECUTE 'SELECT ''' || in_start_time || '''::int, out_value, out_plugin_instance, out_collectd_type FROM ' || temp_table || ';';
+    ELSE
+      RETURN QUERY
+      EXECUTE 'SELECT out_timestamp, out_value, out_plugin_instance, out_collectd_type FROM ' || temp_table || ' WHERE etlid = ''' || this_record.etl_id || ''';';
+    END IF;
+  END LOOP;
 exception when others then
   RAISE NOTICE 'Error select_host_cpu_option_value';
   raise notice '% %', SQLERRM, SQLSTATE;
